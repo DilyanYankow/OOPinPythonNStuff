18ef3443f05eb8371803253be1f7cdd1b95b8b6f
DilyanYankow <xdidox3@abv.bg>
2021-09-23 17:53:11 +0300

    23.09.21

---
+++ daemon_threading.py
@@ -0,0 +1,26 @@
+# Daemon threads = a thread that runs in the background, not important for program to run
+#                   your program will not wait for a daemon thread to complete before exiting
+#                   non daemon threads cannot normally be killed, stay alive until task is complete
+#
+# examples           background task, garbage collection, waiting for input, long running processes
+
+import threading
+import time
+
+
+def timer():
+    print()
+    print()
+    count = 0
+    while True:
+        time.sleep(1)
+        count += 1
+        print("logged in for ", count, " seconds")
+
+
+x = threading.Thread(target=timer, daemon=True)
+x.start()
+
+
+answer = input("Do you wish to exit?\n")
+print()
---
+++ mul_processing.py
@@ -0,0 +1,28 @@
+# multiprocessing = running tasks in a parallel on different CPU cores, bypasses GIL used for threads
+#       multiprocessing = better for cpu bound tasks(heavy CPU usage)
+#       multithreading = better for io bound tasks(waiting around)
+
+from multiprocessing import Process, cpu_count
+import time
+
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+def main():
+    print(cpu_count())
+
+    a = Process(target=counter, args=(100000,))
+    b = Process(target=counter, args=(100000,))
+
+    a.start()
+    b.start()
+
+    a.join()
+    b.join()
+    print("finished in ", time.perf_counter(), "seconds")
+
+if __name__ == "__main__":
+    main()
